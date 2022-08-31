7dd0bf8dc7b578b552f0d271da161720a19009bf
PaulFerdinah <gasymanifika@gmail.com>
2022-08-31 14:09:32 +0300

    initialisation --

--- Training/index.js
+++ Training/index.js
@@ -1,4 +1,4 @@
 const root = document.getElementById("root");
 
-root.innerHTML = "<h1>Hello <h1/>";
-root.innerText += " PEter";
+root.innerHTML =
+  ' Ity le lien <a href="https://demos.creative-tim.com/material-dashboard-pro-react/?_ga=2.200703785.623248287.1661926030-1023394046.1661780042#/dashboards/analytics">Click me </a> ';
